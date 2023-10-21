5d86f0d403ff56c6e791e81c1ba8adc97fc10200
SavvinAnton <savvin19990607@gmail.com>
2023-10-22 02:34:00 +0900

    Added data.json for example

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from VPGen import *
 
 def main():
-    domain = loadGeometry('./test_data.json')
+    domain = loadGeometry('./data.json')
     obstacles, number, porosity, time = generate(domain)
     print('Obstacles:', number, 'Porosity:', round(porosity, 3), 'Time:', round(time, 1))
     plot2d(domain, obstacles, savefig='domain', indent=False)
